35e4e79213a03a912baa4a80d7fe6945412d1cdb
cb <csbutton01@hotmail.com>
2018-06-13 16:41:49 -0700

    polish

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var countDownDate = new Date("Dec 5, 2018 15:37:25").getTime();
+
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("clock").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+});
